716f408b1f81417d97237dbd3c5606b73c9c2542
tajtanvvir1 <tanvirah1223@gmail.com>
2025-05-02 12:22:29 +0600

    Reference Type

---
+++ TypeScript/Module1/1.5_ReerenceType.ts
@@ -0,0 +1,18 @@
+{
+    // Reference Type
+
+    const user:{
+        institute: "Programming Hero"; // literal type (Fixed value)
+        
+        firstName:string;
+        midName?: string; // optional type
+        lastName: string;
+        isStudent : boolean;
+    } ={
+        institute: "Programming Hero",
+        firstName: "Tanvir",
+        midName: "Ahamed",
+        lastName: "Jubayer",
+        isStudent: true
+    }
+}
