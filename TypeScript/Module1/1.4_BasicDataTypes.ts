639546b627ce578334f0fb419c12d3051ae1a27d
tajtanvvir1 <tanvirah1223@gmail.com>
2025-05-02 10:19:10 +0600

    Basic Type Module

--- TypeScript/Module1/1.4_BasicDataTypes.ts
+++ TypeScript/Module1/1.4_BasicDataTypes.ts
@@ -1,20 +1,46 @@
 {
-   // Basic Data Types
-    
-   // String
-   let firstName : string = "Tanvir" 
+  // Basic Data Types
 
-   // number
-   let roll : number = 12
+  //    ### Summary of Types in TypeScript:
 
-   // boolean
-   let isAdmin : boolean = true;
+  // * **Primitive Types**: `string`, `number`, `boolean`, `null`, `undefined`, `symbol`, `bigint`
+  // * **Special Types**: `any`, `unknown`, `void`, `never`
+  // * **Complex Types**: `Array`, `Tuple`, `Object`, `Enum`
+  // * **Advanced Types**: `type` alias, `interface`, `Union`, `Intersection`, `Literal Types`
 
-   // undefined
-   let x : undefined = undefined
+  // String
+  let firstName: string = "Tanvir";
 
-   // null
-   let y : null = null
+  // number
+  let roll: number = 12;
 
-   
+  // boolean
+  let isAdmin: boolean = true;
+
+  // undefined
+  let x: undefined = undefined;
+
+  // null
+  let y: null = null;
+
+  //any - we should not use 
+  // any type allows all types of data
+  let d: any;
+  d = "x";
+  d = 12;
+  d = true;
+
+
+  //Array
+  let friends : string[] = ['Salam', 'Shawon', 'Shojib'];
+
+  let rollList : number[] = [1, 2, 3]
+
+//   friends.push(1);
+// we cannot push another data type
+
+//Tuple - kind of array 
+let info : [string , number, boolean] = ["Tanvir", 27, true]
+
+
 }
